cd7308afcd76be7d642fa65f0749f0889610b82f
tiennguyennesbook <tien.nguyen@nesbook.io>
2022-09-15 13:28:37 -0700

    9/15/2022 add stack series feature

--- protocols.py
+++ protocols.py
@@ -1,9 +1,28 @@
 from pydantic import BaseModel,StrictStr
-from typing import List, Union
-class series_list(BaseModel):
-    series: Union[List[StrictStr],StrictStr]
+from typing import List, Union,Any
+
 class series_id(BaseModel):
-    id:StrictStr
+    id: Union[List[StrictStr],StrictStr]
+
+class catalog(BaseModel):
+    series_id:Any=None
+    series_title:Any=None
+    seasonality:Any = None
+    survey_name:Any=None
+    survey_abbreviation:Any=None
+    measure_data_type: Any=None
+    area:Any=None
+    area_type:Any=None
+
+class series_data(BaseModel):
+    year:Any=None
+    period:Any=None
+    value:Any=None
+    periodName:Any=None
+
+class series_data_type(BaseModel):
+    data_type:Any=None
+
 
 
 
